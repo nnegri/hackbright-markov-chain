1a31b441191f56f75c0a929c9865c5e8df9e9990
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-04 17:50:56 -0700

    Added dictionary, first pass at make_text

--- markov.py
+++ markov.py
@@ -1,6 +1,5 @@
 from random import choice
 
-
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
 
@@ -8,10 +7,11 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    # Open file
     with open(file_path) as text_file:
+        # Read entire file
         text = text_file.read()
-        #print text
+
     return text
 
 
@@ -27,37 +27,62 @@ def make_chains(text_string):
         >>> make_chains("hi there mary hi there juanita")
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
-    chains = {}
+    chains = {} # Create empty dictionary
 
-    words = text_string.split()
+    words = text_string.split() # Split text into list of words
     i = 0
-    for i in range(len(words)-2):
+    for i in range(len(words)-2): # Loop through all words, stopping at second to last
+        # Each key is a tuple of the word at index i, and the word directly following
+        # chains.get returns the value of that tuple; 
+        # it creates an empty list of nothing is there already
         chains[words[i], words[i+1]] = chains.get((words[i], words[i+1]), [])
+        # If there is already a list there, 
+        # it will append the word following the word at index 1 in the tuple
         chains[words[i], words[i+1]].append(words[i+2])
 
-    #print chains
+    print chains
     return chains
 
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    text = ""
+    text = "" # Creates empty string
 
+    # Find a random key(a tuple) from the chains dictionary, and store it in first_key
     first_key = choice(chains.keys()) 
+    # chains.get gives you a list of values for the first_key, 
+    # and choice picks a random one, which we store in first_value
     first_value = choice(chains.get(first_key))
+    # Concatenate both words in the first_key, and the first_value to the text string
     text = first_key[0] +  " " +  first_key[1] + " " + first_value
-    #print text
+    
+    # Loop until a condition breaks it
+    #new_key = (first_key[1], first_value)
 
-    new_key = (first_key[1], first_value)
-    #print new_key
-    for key in chains:
     while True:
+        new_key = (first_key[1], first_value)
+        print new_key
+        print chains.get(new_key)
 
-        new_value = choice(chains.get(new_key))
-        text = text + " " + new_value
-        new_key = 
-        print text
+        if chains.get(new_key) == None:
+            break
+        else:
+            new_value = choice(chains.get(new_key))
+            text = text + " " + new_value
+            
+            first_key = (first_value, new_value)
+
+        # tokens = text.rstrip().split()
+        # text_key = (tokens[-2], tokens[-1])
+
+        # text_value = choice(chains.get(text_key))
+        # text = text + " " + text_value
+
+        # if chains.get(text_key) == []:
+        #     break 
+
+        #break
 
 
     return text
